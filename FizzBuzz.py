0217883d45f6e6f0aaa7b9bd82d507fade12776f
JonPaul Ferzacca <jofe1383@colorado.edu>
2023-06-16 15:14:36 -0600

    Day 3 - Fizz Buzz

---
+++ FizzBuzz.py
@@ -0,0 +1,31 @@
+'''
+Given an integer n, return a string array answer (1-indexed) where:
+answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+answer[i] == "Fizz" if i is divisible by 3.
+answer[i] == "Buzz" if i is divisible by 5.
+answer[i] == i (as a string) if none of the above conditions are true.
+
+Example 1:
+Input: n = 3
+Output: ["1","2","Fizz"]
+'''
+
+class Solution(object):
+    def fizzBuzz(self, n):
+        fblist = [ ]
+        for i in range(1, n+1): 
+            if ((i % 3 == 0) and (i % 5 == 0)):
+                fblist.append("FizzBuzz")
+            elif ((i % 3 == 0) and (i % 5 != 0)):
+                fblist.append("Fizz")
+            elif ((i % 3 != 0) and (i % 5 == 0)):
+                fblist.append("Buzz")
+            else:
+                fblist.append(str(i))
+        return fblist
+
+n = int(input("Enter a number: "))
+solution = Solution()
+result = solution.fizzBuzz(n)
+
+print(result)
--- README.md
+++ README.md
@@ -4,3 +4,9 @@ This Repository is a place to save and and show my progress through 100 Days of 
 
 ### Day 1 - 06/14/2023
 Today I practiced with GitHub by commiting,pushing and pulling directories and files from Visual Studio Code.
+
+### <span style="color:red;"> Day 2 - 06/15/2023  </span>
+Missed a day
+
+### Day 3 - 06/16/2023
+Today I practiced with a simple LeetCode FizzBuzz - Given an integer n, return a string array answer (1-indexed) where:
